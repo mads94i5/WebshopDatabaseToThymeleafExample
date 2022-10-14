1efae3df49190b43e39b46f8c9ff31d7724bd709
mads94i5 <mads94i5@stud.kea.dk>
2022-10-14 18:40:56 +0200

    crud functional

--- src/main/java/com/example/webshopdat22v2/controller/HomeController.java
+++ src/main/java/com/example/webshopdat22v2/controller/HomeController.java
@@ -1,8 +1,9 @@
 package com.example.webshopdat22v2.controller;
+import com.example.webshopdat22v2.model.Product;
 import com.example.webshopdat22v2.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
@@ -15,5 +16,40 @@ public class HomeController {
     public String index(Model model) {
         model.addAttribute("products", productRepository.getAll());
         return "Home/index";
+    }
+
+    @GetMapping("/create")
+    public String showCreateProduct() {
+        return "Home/create";
+    }
+
+    @PostMapping("/create")
+    public String createProduct(@RequestParam("name") String name, @RequestParam("price") int price) {
+        Product newProduct = new Product();
+
+        newProduct.setName(name);
+        newProduct.setPrice(price);
+
+        productRepository.create(newProduct);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/update/{id}")
+    public String showUpdateProduct(@PathVariable("id") int id, Model model) {
+        model.addAttribute("product", productRepository.findById(id));
+        return "Home/update";
+    }
+
+    @PostMapping("/update")
+    public String updateProduct(@ModelAttribute("product") Product product) {
+        productRepository.update(product);
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String showDeleteProduct(@PathVariable("id") int id) {
+        productRepository.deleteById(id);
+        return "redirect:/";
     }
 }
--- src/main/java/com/example/webshopdat22v2/repository/ProductRepository.java
+++ src/main/java/com/example/webshopdat22v2/repository/ProductRepository.java
@@ -1,4 +1,5 @@
 package com.example.webshopdat22v2.repository;
+
 import com.example.webshopdat22v2.model.Product;
 import org.springframework.stereotype.Repository;
 
@@ -12,20 +13,103 @@ public class ProductRepository {
     public List<Product> getAll() {
         List<Product> products = new ArrayList<>();
         try {
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/webshop","root","test");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/webshop", "root", "test");
+
             PreparedStatement psts = conn.prepareStatement("SELECT * FROM product");
-            ResultSet resultSet = psts.executeQuery();
-            while (resultSet.next()) {
-                int id = resultSet.getInt(1);
-                String name = resultSet.getString(2);
-                int price = resultSet.getInt(3);
+
+            ResultSet rs = psts.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+                int price = rs.getInt(3);
                 products.add(new Product(id, name, price));
-                System.out.println("id: " + id + " - name: " + name + " - price: " + price);
             }
         } catch (SQLException e) {
             System.out.println("Cannot connect to database.");
             e.printStackTrace();
         }
         return products;
+    }
+
+    public void create(Product newProduct) {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/webshop", "root", "test");
+
+            String query = "INSERT INTO product (name, price) VALUES (?, ?)";
+            PreparedStatement psts = conn.prepareStatement(query);
+
+            psts.setString(1, newProduct.getName());
+            psts.setInt(2, newProduct.getPrice());
+
+            psts.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Cannot connect to database.");
+            e.printStackTrace();
+        }
+    }
+
+    public Product findById(int id) {
+        Product foundProduct = new Product();
+        foundProduct.setId(id);
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/webshop", "root", "test");
+
+            String query = "SELECT * FROM product WHERE id=?";
+            PreparedStatement psts = conn.prepareStatement(query);
+
+            psts.setInt(1, id);
+
+            ResultSet rs = psts.executeQuery();
+
+            rs.next();
+            String name = rs.getString(2);
+            int price = rs.getInt(3);
+
+            foundProduct.setName(name);
+            foundProduct.setPrice(price);
+
+        } catch (SQLException e) {
+            System.out.println("Cannot connect to database.");
+            e.printStackTrace();
+        }
+        return foundProduct;
+    }
+
+
+
+    public void update(Product product) {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/webshop", "root", "test");
+
+            String query = "UPDATE product " +
+                    "SET name=?, price=? WHERE id=?";
+            PreparedStatement psts = conn.prepareStatement(query);
+
+            psts.setString(1, product.getName());
+            psts.setInt(2, product.getPrice());
+            psts.setInt(3, product.getId());
+
+            psts.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Cannot connect to database.");
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteById(int id) {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/webshop", "root", "test");
+
+            String query = "DELETE FROM product WHERE id=?";
+            PreparedStatement psts = conn.prepareStatement(query);
+
+            psts.setInt(1, id);
+
+            psts.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Cannot connect to database.");
+            e.printStackTrace();
+        }
     }
 }
